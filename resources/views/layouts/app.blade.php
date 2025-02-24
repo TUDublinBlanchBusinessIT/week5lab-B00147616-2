5bf18176f1497bad8ad24206d3c67228d4ac1725
B00147616 Dilnaz Iskandarova <B00147616@mytudublin.ie>
2025-02-24 16:22:15 +0000

    third commit, added new customer and new courts nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,12 @@
               <li class="nav-item">
                 <a class="nav-link" href={{ route('members.index')}}>Members List</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href={{ route('members.create')}}>New Customer</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href={{ route('courts.create')}}>New Courts</a>
+              </li>
             </ul>
           </div>
         </nav>
